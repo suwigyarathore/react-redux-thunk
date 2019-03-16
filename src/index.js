2c5ca0d74b6ebcee9e96399f29eb4e475958f8d6
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-03-16 14:03:28 +0100

    Subscribing on state changes

--- src/index.js
+++ src/index.js
@@ -5,8 +5,20 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import store from "./store";
 
-const state = store.getState();
-ReactDOM.render(<App {...state} />, document.getElementById("root"));
+const render = () => {
+  const state = store.getState();
+  ReactDOM.render(<App {...state} />, document.getElementById("root"));
+};
+
+render();
+store.subscribe(render);
+
+setTimeout(() => {
+  store.dispatch({
+    type: "TODO_ADD",
+    payload: { id: 1, name: "Add new todo", isComplete: true }
+  });
+}, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
