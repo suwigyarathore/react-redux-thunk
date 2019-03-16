c738efb0017c52f3554cba22778701c96dc8bc41
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-03-16 15:26:50 +0100

    Creating reliable action creator

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import store from "./store";
+import { updateCurrent } from "./reducers/todo";
 
-const todoChangeHandler = val =>
-  store.dispatch({ type: "CURRENT_UPDATE", payload: val });
+const todoChangeHandler = val => store.dispatch(updateCurrent(val));
 
 const render = () => {
   const state = store.getState();
