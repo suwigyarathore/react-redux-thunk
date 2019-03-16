2b0840c7356c61cdc4023dadf8af81e24e5af33d
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-03-16 11:54:00 +0100

    Creating global state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const state = {
+  todos: [
+    { id: 1, name: "Render static UI", isComplete: true },
+    { id: 2, name: "Create initial state", isComplete: true },
+    { id: 3, name: "Render based on state", isComplete: false }
+  ]
+};
+ReactDOM.render(<App todos={state.todos} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
