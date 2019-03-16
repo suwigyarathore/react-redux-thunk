ee6859ed4b88590bc5a1503807714304320fde54
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-03-16 13:32:37 +0100

    Adding redux and store

--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import store from "./store";
 
-const state = {
-  todos: [
-    { id: 1, name: "Render static UI", isComplete: true },
-    { id: 2, name: "Create initial state", isComplete: true },
-    { id: 3, name: "Render based on state", isComplete: false }
-  ]
-};
-ReactDOM.render(<App todos={state.todos} />, document.getElementById("root"));
+const state = store.getState();
+ReactDOM.render(<App {...state} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,5 +1,9 @@
 const initState = {
-  todos: []
+  todos: [
+    { id: 1, name: "Render static UI", isComplete: true },
+    { id: 2, name: "Create initial state", isComplete: true },
+    { id: 3, name: "Render based on state", isComplete: false }
+  ]
 };
 export default (state = initState, action) => {
   switch (action.type) {
