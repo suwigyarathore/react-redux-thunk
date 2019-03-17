819ce655a1f84088257fcc2ff9593f927674e275
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-03-17 12:27:15 +0100

    Create react component to display message

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import Message from "./components/Message";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
         <div className="Todo-App">
+          <Message message="Hello mate" />
           <TodoForm />
           <TodoList />
         </div>
