0d1dcdbdd8f095d5f1bf4df6bcfeecc8f423f696
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-03-17 17:36:05 +0100

    Adding router to footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import Message from "./components/Message";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Todo-App">
-          <Message />
-          <TodoForm />
-          <TodoList />
-        </div>
+        <Router>
+          <div className="Todo-App">
+            <Message />
+            <TodoForm />
+            <TodoList />
+            <Footer />
+          </div>
+        </Router>
       </div>
     );
   }
