6c0e2a6f3f1f13232aebd8d7136921fc8eeb789a
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-03-17 12:11:54 +0100

    Setting up redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,5 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import reducer from "./reducers/todo";
 
-export default createStore(reducer, applyMiddleware(thunk));
+export default createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
